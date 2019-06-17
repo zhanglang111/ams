278b8aa19c28e97427d75c5a4808ff3a486e9294
zhanglang111 <1840624227@qq.com>
2019-06-17 15:10:49 +0800

    使用mybatis的generator创建了ams项目。

---
+++ src/main/java/com/ams/ams/mapper/userMapper.java
@@ -0,0 +1,17 @@
+package com.ams.ams.mapper;
+
+import com.ams.ams.entity.user;
+
+public interface userMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(user record);
+
+    int insertSelective(user record);
+
+    user selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(user record);
+
+    int updateByPrimaryKey(user record);
+}
